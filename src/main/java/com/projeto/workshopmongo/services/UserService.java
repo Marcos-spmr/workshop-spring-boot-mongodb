0e3ca46a7f0dab07568072e499e5bd73fece489f
Marcos Ribeiro <mmota5674@gmail.com>
2025-04-10 12:04:11 -0300

    Find user by id

--- src/main/java/com/projeto/workshopmongo/services/UserService.java
+++ src/main/java/com/projeto/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.projeto.workshopmongo.domain.User;
 import com.projeto.workshopmongo.repository.UserRepository;
+import com.projeto.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+	    return repo.findById(id)
+	        .orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado"));
 	}
 }
