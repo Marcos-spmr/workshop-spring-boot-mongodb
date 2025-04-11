41e46348e7fdf59b40c1dc9e1fcad7298396739d
Marcos Ribeiro <mmota5674@gmail.com>
2025-04-11 15:30:52 -0300

    User posts return endpoint

--- src/main/java/com/projeto/workshopmongo/resources/UserResource.java
+++ src/main/java/com/projeto/workshopmongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.projeto.workshopmongo.domain.Post;
 import com.projeto.workshopmongo.domain.User;
 import com.projeto.workshopmongo.dto.UserDTO;
 import com.projeto.workshopmongo.services.UserService;
@@ -64,4 +65,9 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 	}
 	
+	@GetMapping("/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
+	}
 }
